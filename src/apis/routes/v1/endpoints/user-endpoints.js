bccc9f95bff5e5ce13a54534da91621e555d096e
tanstevn <tanstevenlester@gmail.com>
2025-03-25 03:44:14 +0800

    Removed unused files and folders

--- src/apis/routes/v1/endpoints/user-endpoints.js
+++
@@ -1,7 +0,0 @@
-const UserController = require("../../../controllers/user-controller");
-const express = require("express");
-const router = express.Router();
-
-router.get("/list", UserController.getAll);
-
-module.exports = router;
